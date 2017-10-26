7da39e41376aed73a213d6b730b060c4a9b73700
wjt <wujiantao@swad.com>
2017-10-26 11:38:40 +0800

    增加了插槽的demo

--- src/views/app/app.ts
+++ src/views/app/app.ts
@@ -5,11 +5,12 @@ import Vue from "vue";
 const styles = require("./app.scss");
 
 import { Test } from "../../components/test";
+import { SlotComponent } from "../../components/slot-component/slot.component";
 
 @Component({
     name: "entry",
     template: require("./app.html"),
-    components: { Test },
+    components: { Test, SlotComponent },
     computed: {
         styles: function () {
             return styles;
