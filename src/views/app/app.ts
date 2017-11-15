decede47c8d060f17e01a1519fc6c86b73c17ca0
wjt <wujiantao@swad.com>
2017-11-15 13:49:55 +0800

    加入个性化

--- src/views/app/app.ts
+++ src/views/app/app.ts
@@ -2,12 +2,20 @@ import Component from "vue-class-component";
 import Vue from "vue";
 
 import { Btn } from "@components/button/button";
+import { Custom } from "@components/custom/custom";
+// require("./app.scss");
+
 @Component({
     name: "entry",
     components: {
-        Btn
+        Btn, Custom
     },
     template: require("./app.html"),
+    computed: {
+        styles: function () {
+            return "#fa5555";
+        }
+    }
 })
 export class Entry extends Vue {
 
