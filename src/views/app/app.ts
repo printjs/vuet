43b1d725ea634635fc1e2e58a405f6192f7c31ba
wjt <wujiantao@swad.com>
2017-11-15 14:31:41 +0800

    加入拾色器

--- src/views/app/app.ts
+++ src/views/app/app.ts
@@ -1,22 +1,50 @@
 import Component from "vue-class-component";
 import Vue from "vue";
+const VueColor = require("vue-color");
 
 import { Btn } from "@components/button/button";
 import { Custom } from "@components/custom/custom";
-// require("./app.scss");
+
+const defaultProps = {
+    hex: "#194d33",
+    hsl: {
+        h: 150,
+        s: 0.5,
+        l: 0.2,
+        a: 1
+    },
+    hsv: {
+        h: 150,
+        s: 0.66,
+        v: 0.30,
+        a: 1
+    },
+    rgba: {
+        r: 25,
+        g: 77,
+        b: 51,
+        a: 1
+    },
+    a: 1
+};
+const Slider = VueColor.Slider;
 
 @Component({
     name: "entry",
     components: {
-        Btn, Custom
+        Btn, Custom, Slider
+    },
+    methods: {
+        updateValue(color) {
+            console.log(color);
+        }
     },
     template: require("./app.html"),
-    computed: {
-        styles: function () {
-            return "#fa5555";
-        }
-    }
 })
 export class Entry extends Vue {
-
+    data() {
+        return {
+            colors: defaultProps
+        };
+    }
 }
